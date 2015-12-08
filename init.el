ec8a5b3c04479934264d92f69acb08141cb85aaa
juvasquezg <julian.vasquez@netuxtecnologia.com>
2015-12-08 17:14:31 -0500

    Add setup-package.el for melpa repo

--- init.el
+++ init.el
@@ -28,6 +28,11 @@
 ;; Set up appearance early
 (require 'appearance)
 
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
 
-
+;; Setup packages
+(require 'setup-package)
 
